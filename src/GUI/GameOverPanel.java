9697bd135ab161d8577fd2a57668219c1c2aa744
susemeeee <hffg123@naver.com>
2019-12-17 02:30:03 +0900

    update GameOverPanel

--- src/GUI/GameOverPanel.java
+++ src/GUI/GameOverPanel.java
@@ -20,28 +20,39 @@ public class GameOverPanel extends JPanel {
 	private JLabel scoreLabel = new JLabel();
 	private JLabel newRecordLabel = new JLabel("new Record!");
 	private PlaySound playSound = new PlaySound();
+	private JButton rouletteButton = new JButton("·ê ·¿");
+	private JLabel rouletInfoLabel = new JLabel("1È¸ µ¹·Á¼­ Á¡¼öº¯°æ");
 	
 	public GameOverPanel(GameCharacter gameCharacter) throws IOException {
 		setLayout(null);
 		setDefaultPanel(gameCharacter);
-		if(gameCharacter.getCurrentScore() > gameCharacter.getHighScore()) {
-			gameCharacter.setHighScore(gameCharacter.getCurrentScore());
-			GameManager.saveHighScore(gameCharacter.getUserName(), gameCharacter.getCurrentScore());
-			setNewRecordLabel();
-			add(newRecordLabel);
-		}
-		GameManager.sortRanking(gameCharacter.getUserName(), gameCharacter.getCurrentScore());
-		setLayout(null);
 		
-		backButton.setSize(100,40);
-		backButton.setLocation(10, 10);
-		backButton.setFont(new Font("consolas", Font.BOLD, 24));
-		backButton.setContentAreaFilled(false);
+		setLayout(null);
 		
 		backButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
+				if(gameCharacter.getCurrentScore() > gameCharacter.getHighScore()) {
+					gameCharacter.setHighScore(gameCharacter.getCurrentScore());
+					
+					try {
+						GameManager.saveHighScore(gameCharacter.getUserName(), gameCharacter.getCurrentScore());
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+					
+					setNewRecordLabel();
+					add(newRecordLabel);
+				}
+				
+				try {
+					GameManager.sortRanking(gameCharacter.getUserName(), gameCharacter.getCurrentScore());
+				} catch (IOException e2) {
+					e2.printStackTrace();
+				}
+				
 				playSound.playSound("button");
+				
 				try {
 					gameCharacter.setCurrentScore(0);
 					GameFrame.changePanel(MainPanel.class.getName(), gameCharacter);
@@ -51,10 +62,22 @@ public class GameOverPanel extends JPanel {
 			}
 		});
 		
+		rouletteButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				remove(rouletteButton);
+				remove(rouletInfoLabel);
+				revalidate();
+				repaint();
+			}
+		});
+		
 		add(backButton);
 		add(gameOverLabel);
 		add(scoreTitleLabel);
 		add(scoreLabel);
+		add(rouletteButton);
+		add(rouletInfoLabel);
 	}
 	
 	private void setDefaultPanel(GameCharacter gameCharacter) {
@@ -63,14 +86,28 @@ public class GameOverPanel extends JPanel {
 		gameOverLabel.setFont(new Font("±Ã¼­Ã¼", Font.BOLD, 36));
 		
 		scoreTitleLabel.setSize(300, 70);
-		scoreTitleLabel.setLocation(270, 200);
+		scoreTitleLabel.setLocation(270, 250);
 		scoreTitleLabel.setFont(new Font("±Ã¼­Ã¼", Font.BOLD, 24));
 		
 		scoreLabel.setText(Integer.toString(gameCharacter.getCurrentScore()));
 		scoreLabel.setSize(200, 70);
-		scoreLabel.setLocation(320, 200);
+		scoreLabel.setLocation(320, 250);
 		scoreLabel.setFont(new Font("±Ã¼­Ã¼", Font.PLAIN, 24));
 		scoreLabel.setHorizontalAlignment(JLabel.RIGHT);
+		
+		backButton.setSize(100,40);
+		backButton.setLocation(10, 10);
+		backButton.setFont(new Font("consolas", Font.BOLD, 24));
+		backButton.setContentAreaFilled(false);
+		
+		rouletteButton.setSize(100, 40);
+		rouletteButton.setLocation(10, 400);
+		rouletteButton.setFont(new Font("±¼¸²Ã¼", Font.BOLD, 24));
+		rouletteButton.setContentAreaFilled(false);
+		
+		rouletInfoLabel.setSize(400, 70);
+		rouletInfoLabel.setLocation(10, 460);
+		rouletInfoLabel.setFont(new Font("±¼¸²Ã¼", Font.BOLD, 20));
 	}
 	
 	private void setNewRecordLabel() {
