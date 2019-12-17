62ff8767298875af02e8ef1260bd9ceb8006c583
susemeeee <hffg123@naver.com>
2019-12-17 13:57:22 +0900

    change random method

--- src/gameobject/ClearItem.java
+++ src/gameobject/ClearItem.java
@@ -1,13 +1,14 @@
 package gameobject;
 
 import java.awt.Point;
+import java.util.concurrent.ThreadLocalRandom;
 
 import javax.swing.ImageIcon;
 
 public class ClearItem extends FallingObject{
 	public ClearItem() {
 		fallingObjectImage = new ImageIcon("gamefiles/images/clearitem.png");
-		currentPosition = new Point((int)(Math.random() * 800), (int)(Math.random() * (-3000) - 2000));
+		currentPosition = new Point(ThreadLocalRandom.current().nextInt(0, 800), ThreadLocalRandom.current().nextInt(-5000, -2000));
 		sizeRange = new Point(20, 20);
 	}
 }
---
+++ src/gameobject/RandomScoreItem.java
@@ -0,0 +1,14 @@
+package gameobject;
+
+import java.awt.Point;
+import java.util.concurrent.ThreadLocalRandom;
+
+import javax.swing.ImageIcon;
+
+public class RandomScoreItem extends FallingObject{
+	public RandomScoreItem() {
+		fallingObjectImage = new ImageIcon();
+		currentPosition = new Point((int)(ThreadLocalRandom.current().nextInt(0, 800)), ThreadLocalRandom.current().nextInt(-5000, -2000));
+		sizeRange = new Point(20, 20);
+	}
+}
