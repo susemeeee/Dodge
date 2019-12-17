f81d1ea7f72f8d445d7093d193b05a3293391f47
susemeeee <hffg123@naver.com>
2019-12-17 11:51:42 +0900

    update roulette

--- src/GUI/GameOverPanel.java
+++ src/GUI/GameOverPanel.java
@@ -4,6 +4,7 @@ import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
 
 import javax.crypto.spec.GCMParameterSpec;
 import javax.swing.JButton;
@@ -15,6 +16,7 @@ import core.PlaySound;
 import gameobject.GameCharacter;
 
 public class GameOverPanel extends JPanel {
+	private final int MAX_ROULETTE = 3;
 	private JButton backButton = new JButton("Back");
 	private JLabel gameOverLabel = new JLabel("게임 오버");
 	private JLabel scoreTitleLabel = new JLabel("Score: ");
@@ -22,7 +24,9 @@ public class GameOverPanel extends JPanel {
 	private JLabel newRecordLabel = new JLabel("new Record!");
 	private PlaySound playSound = new PlaySound();
 	private JButton rouletteButton = new JButton("룰 렛");
-	private JLabel rouletInfoLabel = new JLabel("1회 돌려서 점수변경");
+	private JLabel rouletteInfoLabel = new JLabel("1회 돌려서 점수변경");
+	private int rouletteCount = 0;
+	private JLabel remainingCountLabel = new JLabel(Integer.toString(MAX_ROULETTE - rouletteCount) + " 회 남음");
 	
 	public GameOverPanel(GameCharacter gameCharacter) throws IOException {
 		setLayout(null);
@@ -66,13 +70,27 @@ public class GameOverPanel extends JPanel {
 		rouletteButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
-				rouletteScore(gameCharacter);
-				scoreLabel.setText(Integer.toString(gameCharacter.getCurrentScore()));
-				
-				remove(rouletteButton);
-				remove(rouletInfoLabel);
-				revalidate();
-				repaint();
+				rouletteCount++;
+				if(rouletteCount < MAX_ROULETTE) {
+					rouletteScore(gameCharacter);
+					scoreLabel.setText(Integer.toString(gameCharacter.getCurrentScore()));
+					remainingCountLabel.setText(Integer.toString(MAX_ROULETTE - rouletteCount) + " 회 남음");
+					
+					revalidate();
+					repaint();
+				}
+				else {
+					rouletteScore(gameCharacter);
+					scoreLabel.setText(Integer.toString(gameCharacter.getCurrentScore()));
+					remainingCountLabel.setText(Integer.toString(MAX_ROULETTE - rouletteCount) + " 회 남음");
+					
+					remove(rouletteButton);
+					remove(rouletteInfoLabel);
+					remove(remainingCountLabel);
+					
+					revalidate();
+					repaint();
+				}
 			}
 		});
 		
@@ -81,7 +99,8 @@ public class GameOverPanel extends JPanel {
 		add(scoreTitleLabel);
 		add(scoreLabel);
 		add(rouletteButton);
-		add(rouletInfoLabel);
+		add(rouletteInfoLabel);
+		add(remainingCountLabel);
 	}
 	
 	private void setDefaultPanel(GameCharacter gameCharacter) {
@@ -109,9 +128,13 @@ public class GameOverPanel extends JPanel {
 		rouletteButton.setFont(new Font("굴림체", Font.BOLD, 24));
 		rouletteButton.setContentAreaFilled(false);
 		
-		rouletInfoLabel.setSize(400, 70);
-		rouletInfoLabel.setLocation(10, 460);
-		rouletInfoLabel.setFont(new Font("굴림체", Font.BOLD, 20));
+		rouletteInfoLabel.setSize(400, 70);
+		rouletteInfoLabel.setLocation(10, 460);
+		rouletteInfoLabel.setFont(new Font("굴림체", Font.BOLD, 20));
+		
+		remainingCountLabel.setSize(400, 70);
+		remainingCountLabel.setLocation(10, 500);
+		remainingCountLabel.setFont(new Font("굴림체", Font.BOLD, 20));
 	}
 	
 	private void setNewRecordLabel() {
@@ -121,7 +144,7 @@ public class GameOverPanel extends JPanel {
 	}
 	
 	private void rouletteScore(GameCharacter gameCharacter) {
-		double magnification = Math.random() * 1.2 + 0.4;
+		double magnification = ThreadLocalRandom.current().nextDouble(0.4, 1.6);
 		gameCharacter.setCurrentScore((int)(gameCharacter.getCurrentScore() * magnification));
 		System.out.println(gameCharacter.getCurrentScore());
 	}
